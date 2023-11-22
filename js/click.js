6a7707e68306dbdad9f81f2466c8883a3a5acc0c
NaveenJohnPremkumar <njpremk.16@gmail.com>
2023-11-21 20:52:00 -0500

    webview

---
+++ js/click.js
@@ -0,0 +1,19 @@
+let clickable = document.body.querySelectorAll('#like');
+
+let isClicked = new Array(clickable.length).fill(false);
+
+let liked = 0;
+
+for(var i = 0; i < clickable.length; i++) {
+    clickable[i].addEventListener("click", function(i) {
+        if (!isClicked[i]) {
+            isClicked[i] = true;
+            liked++;
+            alert("You liked a post!");
+        }else {
+            alert("You already liked this post!");
+        }
+        console.log(document.body.querySelector('.liked').innerHTML);
+        document.body.querySelector('.liked').innerHTML = ("Posts liked: " + liked) ;
+    });
+}
